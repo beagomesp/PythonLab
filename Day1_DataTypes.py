874cb60202a7dc4664251ad2c967e1afc3d994c3
Bea <49660676+beagomesp@users.noreply.github.com>
2020-09-14 11:21:43 -0300

    Add files via upload

---
+++ Day1_DataTypes.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+d = 4.0
+s = "HackerRank "
+# Read and save an integer, double, and String to your variables.
+variavelint = int(input())
+variaveldouble = float(input())
+variavelstring = input()
+# Print the sum of both integer variables on a new line.
+print (i + variavelint)
+# Print the sum of the double variables on a new line.
+print (d + variaveldouble)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print (s + variavelstring)

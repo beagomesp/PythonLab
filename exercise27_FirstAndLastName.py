75c6ea4d33d81f586dc423c46ffc7f7483762609
Bea <49660676+beagomesp@users.noreply.github.com>
2021-02-28 16:30:14 -0300

    Add files via upload

---
+++ exercise27_FirstAndLastName.py
@@ -0,0 +1,5 @@
+frase = str(input('Digite seu nome completo: ')).strip()
+nome = frase.split()
+print('Muito prazer em te conhecer!')
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu último nome é {}'.format(nome[len(nome)-1]))
